7c20d273b142987b9d402d8c4c34337e103bd029
Antoine Desportes <developer@Antoines-MacBook-Pro.local>
2025-12-10 11:17:19 +0100

    vortex_revenuecat_initializer: use external API key

--- ios/chrome/app/vortex_revenuecat_initializer.mm
+++ ios/chrome/app/vortex_revenuecat_initializer.mm
@@ -1,16 +1,11 @@
 #import "ios/chrome/app/vortex_revenuecat_initializer.h"
 #import "third_party/revenuecat/ios/vortex_revenuecat_shim.h"
+#import "ios/third_party/vortex/src/vortex_constants.h"
 
 @implementation VortexRevenueCatInitializer
 
 + (void)configureRevenueCat {
-#if DEBUG
-    NSString *apiKey = @"appl_wEdjzBbxSyYcdvkWJEqFlEpRoAt";
-#else
-    NSString *apiKey = @"appl_wEdjzBbxSyYcdvkWJEqFlEpRoAt";
-#endif
-    // For now you can use anonymous users (nil appUserID) or derive from Chrome profile later.
-    [VortexRevenueCatShim configureWithApiKey:apiKey appUserID:nil];
+    [VortexRevenueCatShim configureWithApiKey:VORTEX_REVENUECAT_API_KEY appUserID:nil];
 }
 
 @end
