1776ff89aa2178586d68462d31b46bc1c73662cd
antoinedesp <antoinedesportes77@gmail.com>
2025-12-23 00:33:23 +0100

    browser: shared: model: prefs: add ios.adblocker.enabled

--- ios/chrome/browser/shared/model/prefs/pref_names.h
+++ ios/chrome/browser/shared/model/prefs/pref_names.h
@@ -7,6 +7,9 @@
 
 namespace prefs {
 
+// Boolean that is true when AdBlocker is enabled.
+inline constexpr char kAdBlockerEnabled[] = "ios.adblocker.enabled";
+
 // Number of times the "Address Bar" settings "new" IPH badge has been shown.
 // This is set to INT_MAX when the user visites the "Address Bar" settings page.
 inline constexpr char kAddressBarSettingsNewBadgeShownCount[] =
