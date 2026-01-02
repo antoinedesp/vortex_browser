538b54237c759488f544ff80ee1f12dd24cadb49
antoinedesp <antoinedesportes77@gmail.com>
2026-01-02 17:56:26 +0100

    vortex_vpn_servers: add vortex_vpn_server

---
+++ ios/chrome/browser/ui/vortex_vpn_servers/vortex_vpn_server.h
@@ -0,0 +1,58 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_VORTEX_VPN_SERVERS_VORTEX_VPN_SERVER_H_
+#define IOS_CHROME_BROWSER_UI_VORTEX_VPN_SERVERS_VORTEX_VPN_SERVER_H_
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+// Model object representing a VPN server from the Vortex API.
+@interface VortexVPNServer : NSObject
+
+// Server ID from the API.
+@property(nonatomic, assign) NSInteger serverId;
+
+// Unique identifier for the server.
+@property(nonatomic, copy) NSString* uuid;
+
+// Server IP address.
+@property(nonatomic, copy) NSString* ip;
+
+// Server port (0 means default).
+@property(nonatomic, assign) NSInteger port;
+
+// Username for authentication.
+@property(nonatomic, copy) NSString* username;
+
+// Password for authentication.
+@property(nonatomic, copy) NSString* password;
+
+// Whether the server is currently online.
+@property(nonatomic, assign) BOOL isOnline;
+
+// Whether this is a premium server.
+@property(nonatomic, assign) BOOL isPremium;
+
+// Country code (e.g., "nl", "us").
+@property(nonatomic, copy) NSString* country;
+
+// Pre-shared key for IKEv2.
+@property(nonatomic, copy) NSString* psk;
+
+// Optional flag URL/emoji.
+@property(nonatomic, copy) NSString* flag;
+
+// Creates a VortexVPNServer from an API response dictionary.
++ (nullable instancetype)serverFromDictionary:(NSDictionary*)dict;
+
+// Returns the display name for the server's country.
+- (NSString*)countryDisplayName;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // IOS_CHROME_BROWSER_UI_VORTEX_VPN_SERVERS_VORTEX_VPN_SERVER_H_
---
+++ ios/chrome/browser/ui/vortex_vpn_servers/vortex_vpn_server.mm
@@ -0,0 +1,127 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/vortex_vpn_servers/vortex_vpn_server.h"
+
+@implementation VortexVPNServer
+
++ (nullable instancetype)serverFromDictionary:(NSDictionary*)dict {
+  if (![dict isKindOfClass:[NSDictionary class]]) {
+    return nil;
+  }
+
+  VortexVPNServer* server = [[VortexVPNServer alloc] init];
+
+  // Parse required fields
+  id serverId = dict[@"id"];
+  if ([serverId respondsToSelector:@selector(integerValue)]) {
+    server.serverId = [serverId integerValue];
+  }
+
+  id uuid = dict[@"uuid"];
+  if ([uuid isKindOfClass:[NSString class]]) {
+    server.uuid = uuid;
+  }
+
+  id ip = dict[@"ip"];
+  if ([ip isKindOfClass:[NSString class]]) {
+    server.ip = ip;
+  } else {
+    // IP is required
+    return nil;
+  }
+
+  id port = dict[@"port"];
+  if ([port respondsToSelector:@selector(integerValue)]) {
+    server.port = [port integerValue];
+  }
+
+  id username = dict[@"username"];
+  if ([username isKindOfClass:[NSString class]]) {
+    server.username = username;
+  }
+
+  id password = dict[@"password"];
+  if ([password isKindOfClass:[NSString class]]) {
+    server.password = password;
+  }
+
+  id isOnline = dict[@"is_online"];
+  if ([isOnline respondsToSelector:@selector(boolValue)]) {
+    server.isOnline = [isOnline boolValue];
+  }
+
+  id isPremium = dict[@"is_premium"];
+  if ([isPremium respondsToSelector:@selector(boolValue)]) {
+    server.isPremium = [isPremium boolValue];
+  }
+
+  id country = dict[@"country"];
+  if ([country isKindOfClass:[NSString class]]) {
+    server.country = country;
+  }
+
+  id psk = dict[@"psk"];
+  if ([psk isKindOfClass:[NSString class]]) {
+    server.psk = psk;
+  } else {
+    // PSK is required
+    return nil;
+  }
+
+  id flag = dict[@"flag"];
+  if ([flag isKindOfClass:[NSString class]]) {
+    server.flag = flag;
+  }
+
+  return server;
+}
+
+- (NSString*)countryDisplayName {
+  if (!self.country || self.country.length == 0) {
+    return @"Unknown";
+  }
+
+  // Map country codes to display names
+  static NSDictionary<NSString*, NSString*>* countryNames = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    countryNames = @{
+      @"us" : @"United States",
+      @"nl" : @"Netherlands",
+      @"de" : @"Germany",
+      @"gb" : @"United Kingdom",
+      @"uk" : @"United Kingdom",
+      @"fr" : @"France",
+      @"ca" : @"Canada",
+      @"au" : @"Australia",
+      @"jp" : @"Japan",
+      @"sg" : @"Singapore",
+      @"ch" : @"Switzerland",
+      @"se" : @"Sweden",
+      @"no" : @"Norway",
+      @"dk" : @"Denmark",
+      @"fi" : @"Finland",
+      @"it" : @"Italy",
+      @"es" : @"Spain",
+      @"br" : @"Brazil",
+      @"mx" : @"Mexico",
+      @"in" : @"India",
+      @"kr" : @"South Korea",
+      @"hk" : @"Hong Kong",
+      @"ie" : @"Ireland",
+      @"at" : @"Austria",
+      @"be" : @"Belgium",
+      @"pl" : @"Poland",
+      @"cz" : @"Czech Republic",
+      @"nz" : @"New Zealand",
+      @"za" : @"South Africa",
+    };
+  });
+
+  NSString* displayName = countryNames[self.country.lowercaseString];
+  return displayName ?: self.country.uppercaseString;
+}
+
+@end
