7572dc058d264c0417f3a9ffbde82cff7af2929b
antoinedesp <antoinedesportes77@gmail.com>
2026-01-13 10:46:12 +0100

    chrome: app: vortex_revenuecat_initializer: add firebase appAPpInstanceID

--- ios/chrome/app/vortex_revenuecat_initializer.mm
+++ ios/chrome/app/vortex_revenuecat_initializer.mm
@@ -1,11 +1,25 @@
 #import "ios/chrome/app/vortex_revenuecat_initializer.h"
 #import "third_party/revenuecat/ios/vortex_revenuecat_shim.h"
+#import "third_party/firebase/ios/vortex_firebase_analytics_shim.h"
 #import "ios/third_party/vortex/src/vortex_constants.h"
 
 @implementation VortexRevenueCatInitializer
 
 + (void)configureRevenueCat {
+    // Configure RevenueCat SDK
     [VortexRevenueCatShim configureWithApiKey:VORTEX_REVENUECAT_API_KEY appUserID:nil];
+
+    // Link Firebase Analytics with RevenueCat for server-side event tracking.
+    // This enables RevenueCat to send subscription lifecycle events
+    // (purchases, trials, renewals, cancellations, etc.) to Google Analytics.
+    // See: https://www.revenuecat.com/docs/integrations/third-party-integrations/firebase-integration
+    NSString *firebaseAppInstanceID = [VortexFirebaseAnalyticsShim appInstanceID];
+    if (firebaseAppInstanceID) {
+        [VortexRevenueCatShim setFirebaseAppInstanceID:firebaseAppInstanceID];
+    } else {
+        NSLog(@"[VortexRevenueCatInitializer] WARNING: Firebase App Instance ID not available. "
+              @"Ensure Firebase is configured before RevenueCat.");
+    }
 }
 
 @end
